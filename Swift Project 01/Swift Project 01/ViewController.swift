5555ba84dd021f6ac2c97781669a51a59f125eab
Tommy Lau <tommy@gen-new.com>
2016-10-01 22:26:49 +0800

    Initial commit with "Project 01"

---
+++ Swift Project 01/Swift Project 01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Project 01
+//
+//  Created by Tommy Lau on 01/10/2016.
+//  Copyright © 2016 Tommy Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
