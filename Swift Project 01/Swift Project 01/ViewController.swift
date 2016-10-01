d51d7ef6078c9c619d99bd81e7555cca385c9b4e
Tommy Lau <tommy@gen-new.com>
2016-10-02 01:20:54 +0800

    Project 01 - TAP COUNTER

--- Swift Project 01/Swift Project 01/ViewController.swift
+++ Swift Project 01/Swift Project 01/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+
+    var count = 0
+
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        count = count + 1
+        countLabel.text = String(count)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
